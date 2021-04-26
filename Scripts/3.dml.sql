0a63d01bab8d14b951a997d6a283f9c4684d9a6f
MyungHunSong <change1677@naver.com>
2021-04-26 11:23:30 +0900

    action/dto 추가

--- Scripts/3.dml.sql
+++ Scripts/3.dml.sql
@@ -6,3 +6,4 @@ insert into dog(kind,price,image,country,height,weight,content,readcount) values
 	('진도개', 3000,'jin.jpg','대한민국',1,20,'최고다',0),
 	('허스키', 4000,'h.jpg','북극',1,20,'멋지다',0);
 	
+select * from dog;
